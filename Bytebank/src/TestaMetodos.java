f757f32ce5e57d2b27a6cbb51a6ea8a5ff4852b7
Peterson <petersonros@hotmail.com>
2022-06-09 09:54:26 -0300

    testando metodos

--- Bytebank/src/TestaMetodos.java
+++ Bytebank/src/TestaMetodos.java
@@ -5,5 +5,22 @@ public class TestaMetodos {
 		contaDoPaulo.saldo = 100;
 		contaDoPaulo.deposita(50);
 		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+        boolean sucessoTransferencia = contaDaMarcela.transfere(3000, contaDoPaulo);
+        if(sucessoTransferencia) {
+            System.out.println("transferencia com sucesso");
+        } else {
+            System.out.println("faltou dinheiro");
+
+        }
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
---
+++ sintaxe-variaveis-fluxo/src/Conta.java
@@ -0,0 +1,9 @@
+
+public class Conta {
+	double saldo;
+	int numero;
+	
+	void desposita(double valor) {
+		this.saldo = this.saldo + valor;
+	}
+}
