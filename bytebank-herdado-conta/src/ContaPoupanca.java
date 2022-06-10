dccd616eb21ec6a1d714517eb48f588de7c7de10
Peterson <petersonros@hotmail.com>
2022-06-10 20:49:55 -0300

    testando classe abstrata

---
+++ bytebank-herdado-conta/src/ContaPoupanca.java
@@ -0,0 +1,11 @@
+
+public class ContaPoupanca {
+	public ContaPoupanca(int agencia, int numero){
+	    super(agencia, numero);
+	}
+
+	public void deposita(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -1,14 +1,12 @@
 
 public class TesteFuncionario {
 	public static void main(String[] args) {
-		Funcionario nico = new Funcionario();
+		Funcionario nico = new Gerente();
 		nico.setNome("Nico Steppat");
 		nico.setCpf("223335646-9");
 		nico.setSalario(2600);
 		
 		System.out.println(nico.getNome());
 		System.out.println(nico.getBonificacao());
-		
-		
 	}
 }
