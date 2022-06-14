693fae548436a3abf6b4ef7f0a7e2ad4e38b2d05
Peterson <petersonros@hotmail.com>
2022-06-13 21:29:46 -0300

    update

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,6 +1,20 @@
 
-public class ContaCorrente extends Conta {
+
+// new conta Contacorrente()
+public class ContaCorrente extends Conta .implements Tributavel {
+	
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
+	}
+	
+	@Overrride
+	public boolean saca(double valor) {
+		double valorSacar = valor + 0.2;
+		return super .saca(valorASacar);
+	}
+	
+	@Overrride
+	public void deposita(double valor) {
+		super.saldo += valor;
 	}
 }
