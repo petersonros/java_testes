5a52e5fffbb5674f9b0a47ca62428a003ee530d9
Peterson <petersonros@hotmail.com>
2022-06-15 22:07:12 -0300

    update TesteGerente.java

--- bytebank-herdado/src/TesteGerente.java
+++ bytebank-herdado/src/TesteGerente.java
@@ -1,17 +1,21 @@
 
 public class TesteGerente {
+	
 	public static void main(String[] args) {
+		
+		Autenticavel referencia = new Cliente();
+		
 		Gerente g1 = new Gerente();
-		g1.setNome("Marco");
-		g1.setCpf("23556813");
-		g1.setSalario(5000.0);
+		g1.setNome(nome: "Marco");
+		g1.setCpf(cpf: "23556813");
+		g1.setSalario(salario; 5000.0);
 		
 		System.out.println(g1.getNome());
 		System.out.println(g1.getCpf());
 		System.out.println(g1.getSalario());
 		
-		g1.setSenha(2222);
-		boolean autenticou = g1.autentica(2222);
+		g1.setSenha(senha: 2222);
+		boolean autenticou = g1.autentica(senha: 2222);
 		
 		System.out.println(autenticou);
 		
