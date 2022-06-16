dab325b65d7a04df63ea309a5f6a3eb522610ed7
Peterson <petersonros@hotmail.com>
2022-06-15 21:39:29 -0300

    nova classe Cliente.java

--- bytebank-herdado/src/Autenticavel.java
+++ bytebank-herdado/src/Autenticavel.java
@@ -1,4 +1,13 @@
 
+// contrato Autenticavel
+	// quem assina esse contrato, precisa implmentar
+		// metodo setSenha
+		// metodo autentica
+
 public class Autenticavel {
+	
+	public abstract void setSenha(int senha) ;
+	
+	public abstract boolean autentica(int senha) ;
 
 }
