b1db4cdc51298c7d4f02e89b418d5f3d3be01ee3
Peterson <petersonros@hotmail.com>
2022-06-17 14:32:01 -0300

    update

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestaArrayReferenciasTypeCast.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestaArrayReferenciasTypeCast.java
@@ -10,22 +10,24 @@ public class TestaArrayReferenciasTypeCast {
 	public static void main(String[] args) {
 		
 		// Conta[] contas = new Conta[5]; // especifico
-		Object[] contas = new Object[5]; // generico
+		Object[] referencias = new Object[5]; // generico
 		
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 		
 		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
-		contas[1] = cc2;
+		referencias[1] = cc2;
 		
 		Cliente cliente = new Cliente();
-		contas[2] = cliente;
+		referencias[2] = cliente;
 		
 		// System.out.println(cc2.getNumero());
 		
-		System.out.println(contas[1].getNumero());
+//		Object referenciaGenerica = contas[1];
+//		
+//		System.out.println(contas[1].getNumero());
 		
-		ContaPoupanca ref = (ContaPoupanca) contas[1]; // type Cast
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; // type Cast
 		
 		// ambas aponta para o mesmo objeto
 		System.out.println(cc2.getNumero()); 
