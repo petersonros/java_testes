74b159576046532d6d27a697a62cee9d05891da4
Peterson <petersonros@hotmail.com>
2022-06-17 15:21:47 -0300

    update

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
@@ -11,7 +11,7 @@ public class TesteContas {
 	public static void main(String[] args) throws SaldoInsuficienteException{
 						
 		ContaCorrente cc = new ContaCorrente(111, 111);
-		cc.deposita(100.0);
+		cc.deposita(150.0);
 		
 		ContaPoupanca cp = new ContaPoupanca(222, 222);
 		cp.deposita(200.0);
