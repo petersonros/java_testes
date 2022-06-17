d36f4b371dde6a6b5f4b10a7eba83a43b3d9cc1e
Peterson <petersonros@hotmail.com>
2022-06-16 22:35:32 -0300

    testando array com laço for

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -7,9 +7,17 @@ public class Teste {
 		
 		int[] idades = new int[5]; // inicia com indice 0
 		
-		int idade1 = idades[0];
+		idades[0] = 29;
+		idades[1] = 39;
+		idades[2] = 49;
+		idades[3] = 59;
+		idades[4] = 69;
+		
+		int idade1 = idades[4];
 		
 		System.out.println(idade1);
+		
+		System.out.println(idades.length);
 	}
 
 }
