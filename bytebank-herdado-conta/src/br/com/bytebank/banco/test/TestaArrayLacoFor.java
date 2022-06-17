b9ef6e18526fa86642f631c884d0fa9573dea19b
Peterson <petersonros@hotmail.com>
2022-06-16 22:40:55 -0300

    testanto laço for na classe TestaArrayLacoFor.java

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestaArrayLacoFor.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestaArrayLacoFor.java
@@ -3,8 +3,18 @@ package br.com.bytebank.banco.test;
 public class TestaArrayLacoFor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//  testando array com laço for
+		
+		int[] idades = new int[5];
+		
+		for(int i = 0; i < idades.length; i++ ) {
+			idades[i] = i * i;
+		}
+		
+		for(int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+		
 	}
 
 }
