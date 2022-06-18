a09b8f545cc0fff9bf05fa8e8f2521f9ad703504
Peterson <petersonros@hotmail.com>
2022-06-18 20:49:28 -0300

    update

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,23 +1,27 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contas = new ContaCorrente[5];	
+		Conta[] contas = new Conta[5];	
 		ContaCorrente cc1 = new ContaCorrente(22, 11);
 		contas[0] = cc1;
 		
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
+		// cria instância de ContaPoupanca
+		ContaPoupanca cc2 = new ContaPoupanca(22, 22);
 		contas[1] = cc2;
 		
 		// System.out.println(cc2.getNumero());
 		
 		System.out.println(contas[1].getNumero());
 		
-		ContaCorrente ref = contas[1];
+		// alterou o tipo, realizando o cast
+		ContaCorrente ref = (ContaCorrente) contas[0];
 		
 		// ambas aponta para o mesmo objeto
 		System.out.println(cc2.getNumero()); 
