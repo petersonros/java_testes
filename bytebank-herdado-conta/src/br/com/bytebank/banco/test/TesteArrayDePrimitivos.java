d454d5fa21ca4748bfa6f9be3fa8706841ecfee7
Peterson <petersonros@hotmail.com>
2022-06-19 14:43:29 -0300

    novos testes e classes, GuardadorDeContas...

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -2,19 +2,19 @@ package br.com.bytebank.banco.test;
 
 public class TesteArrayDePrimitivos {
 
+	//Array []
 	public static void main(String[] args) {
-		//  testando array com laço for
 		
-		int[] idades = new int[5];
+		int[] idades = new int[5];//inicializa o array com 0
 		
-		for(int i = 0; i < idades.length; i++ ) {
+		for(int i = 0; i < idades.length; i++) {
 			idades[i] = i * i;
 		}
 		
 		for(int i = 0; i < idades.length; i++) {
 			System.out.println(idades[i]);
 		}
-		
+
 	}
 
 }
