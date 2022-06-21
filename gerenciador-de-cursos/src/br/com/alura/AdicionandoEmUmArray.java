e8288f236ae3e7bc60f83047caa17ce2dc138a4d
Peterson <petersonros@hotmail.com>
2022-06-21 16:35:49 -0300

    finalisando TestandoListas.java, concluindo exercicio AdicionandoEmUmArray.java

---
+++ gerenciador-de-cursos/src/br/com/alura/AdicionandoEmUmArray.java
@@ -0,0 +1,36 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class AdicionandoEmUmArray {
+
+	public static void main(String[] args) {
+		
+		// Adicionando elementos em um ArrayList
+		String curso1 = "Java 8: Tire proveito dos novos recursos da linguagem";
+		String curso2 = "Apache Camel";
+		String curso3 = "Certificado Java SE 8 Programer I";
+		
+		ArrayList<String> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
+		
+		System.out.println(cursos);
+		
+		// Removendo elementos de uma lista
+//		cursos.remove(1);
+//		System.out.println(cursos);
+		
+		// Acessando elementos da lista
+		String primeiroCurso = cursos.get(0);
+		System.out.println("Primeiro curso da lista: " + primeiroCurso);
+		 // System.out.println("Primeiro curso da lista: "+ cursos.get(0));
+		
+		// Ordenar lista por ordem alfabetica
+		Collections.sort(cursos);
+		System.out.println("Cursos em ordem alfabetica: " + cursos);
+	}
+}
--- gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
+++ gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -1,5 +1,62 @@
 package br.com.alura;
 
-public class TestandoListas {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class TestandoListas {
+	
+	public static void main(String[] args) {
+		String aula1 = "Conhecendo mais de listas.";
+		String aula2 = "Modelando a classe Aula.";
+		String aula3 = "Trabalhando com Curso e Sets.";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		System.out.println(aulas);
+		// [Conhecendo mais de listas., Modelando a classe Aula., Trabalhando com Curso e Sets.]
+		
+		aulas.remove(0);
+		System.out.println(aulas);
+		// [Modelando a classe Aula., Trabalhando com Curso e Sets.]
+		
+		String primeiraAula = aulas.get(0);
+		System.out.println("A primeira aula é " + primeiraAula);
+		// A primeira aul é Modelando a classe Aula.
+		
+		for (int i = 0; i < aulas.size(); i++) {
+			System.out.println("aula : " + aulas.get(i));
+		}
+		// aula : Modelando a classe Aula.
+		// aula : Trabalhando com Curso e Sets.
+		
+		System.out.println(aulas.size());
+		// 2
+		
+ // duas formas diferentes de laços for...
+		aulas.forEach(aula -> {
+			System.out.println("percorrendo: ");
+			System.out.println("Aula " + aula);
+		});
+		// percorrendo: 
+		// Aula Modelando a classe Aula.
+		// percorrendo: 
+		// Aula Trabalhando com Curso e Sets.
+		
+		for (String aula : aulas) {
+			System.out.println("Aula: " + aula);
+		}
+		// Aula: Modelando a classe Aula.
+		// Aula: Trabalhando com Curso e Sets.
+		
+		aulas.add("Aumentando o nosso conhecimento");
+		System.out.println(aulas);
+		// [Modelando a classe Aula., Trabalhando com Curso e Sets., aumentando o nosso conhecimento]
+		
+		Collections.sort(aulas);
+		System.out.println("Depois de ordenado: ");
+		System.out.println(aulas);
+		
+	}
 }
