fd291cf6d4a7571b1dd0f60cb8862c536dcc55e7
Peterson <petersonros@hotmail.com>
2022-06-23 15:52:38 -0300

    comentando o codigo na classe Aluno

--- gerenciador-de-cursos/src/br/com/alura/Aluno.java
+++ gerenciador-de-cursos/src/br/com/alura/Aluno.java
@@ -2,22 +2,27 @@ package br.com.alura;
 
 public class Aluno {
 
+	// atributos da classe Aluno
 	private String nome;
 	private int numeroMatricula;
 
+	// construtor
 	public Aluno(String nome, int numeroMatricula) {
 		this.nome = nome;
 		this.numeroMatricula = numeroMatricula;
 	}
 
+	// getter
 	public String getNome() {
 		return nome;
 	}
 
+	// getter
 	public int getNumeroMatricula() {
 		return numeroMatricula;
 	}
 
+	// metodo
 	@Override
 	public String toString() {
 		return "[Aluno: " + this.nome + ", matricula:" + this.numeroMatricula + "]";
