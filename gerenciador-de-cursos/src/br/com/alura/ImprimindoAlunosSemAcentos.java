b65f13b093ee391ec2f5637e1994c413ddee19bb
Peterson <petersonros@hotmail.com>
2022-06-23 10:45:07 -0300

    incluindo alunos e contando o tamanho do set

--- gerenciador-de-cursos/src/br/com/alura/ImprimindoAlunosSemAcentos.java
+++ gerenciador-de-cursos/src/br/com/alura/ImprimindoAlunosSemAcentos.java
@@ -4,17 +4,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ImprimindoAlunosSemAcentos {
-	
+
 	public static void main(String[] args) {
 		Set<String> alunos = new HashSet<>();
 		alunos.add("Paulo");
 		alunos.add("Alberto");
 		alunos.add("Pedro");
 		alunos.add("Nico");
+
+		System.out.println(alunos.size());
+
+		alunos.add("Pedro");
 		
+		boolean adicionou = alunos.add("Pedro");
+		System.out.println("Pedro foi adicionado ao Set? " + adicionou);
+
+		adicionou = alunos.add("Peterson");
+		System.out.println("Peterson foi adicionado ao Set? " + adicionou);
+
 		for (String aluno : alunos) {
 			System.out.println(aluno);
 		}
+		
+		System.out.println(alunos.size());
+
 	}
 
 }
