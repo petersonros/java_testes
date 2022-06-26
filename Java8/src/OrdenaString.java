1d8a4e3220df2e18f43867bbfdc95990f15bfef3
Peterson <petersonros@hotmail.com>
2022-06-26 20:34:30 -0300

    testando o método sort

--- Java8/src/OrdenaString.java
+++ Java8/src/OrdenaString.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class OrdenaString {
 
 	public static void main(String[] args) {
 
-		List<String>
-
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+		
+		Collections.sort(palavras);
+		System.out.println(palavras);
+			
 	}
 
 }
