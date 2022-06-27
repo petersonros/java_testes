29914713a11ba0a7e89d4a9462a594fe041add30
Peterson <petersonros@hotmail.com>
2022-06-27 10:32:38 -0300

    nova classe de testes: OrdenaString

--- Java8/src/OrdenaString.java
+++ Java8/src/OrdenaString.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaString {
 
@@ -10,10 +11,37 @@ public class OrdenaString {
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
-		
-		Collections.sort(palavras);
+
+		Comparator<String> comparador = new ComparadorPorTAmanho();
+		// Collections.sort(palavras, comparador); // até o Java7 era assim
+		palavras.sort(comparador); // a partir do Java8 ficou assim
 		System.out.println(palavras);
-			
+
+//		for(String p: palavras) {
+//			System.out.println(p);
+//		} // formato antigo
+
+		Consumer<String> consumidor = null;
+		palavras.forEach(consumidor);
+	}
+
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+	}
+}
+
+class ComparadorPorTAmanho implements Comparator<String> {
+	@Override
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length())
+			return -1;
+		if (s1.length() > s2.length())
+			return 1;
+		return 0;
 	}
 
 }
